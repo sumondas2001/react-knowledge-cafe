d177190510ccbfd7c166468ad021a07cf2af4d17
sumondas2001 <sumonchandradas1010@gmail.com>
2024-05-20 09:44:06 +0600

    json data load and set use state complited

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+     return (
+          <div>
+               <h1 className='font-bold text-3xl'>Knowledge Cafe</h1>
+
+          </div>
+     );
+};
+
+export default Header;
