237ae84eb5ce1e598ff8507494c03bcf229922e5
sumondas2001 <sumonchandradas1010@gmail.com>
2024-05-20 11:27:43 +0600

    main section complited

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,11 +1,33 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({ blog }) => {
+     const { cover_img, title, author_img, reading_time, author, posted_date, hashtags } = blog
      console.log(blog)
      return (
-          <div>
-
-          </div>
+          // main section 
+          <main>
+               <img className='w-3/5 rounded-xl' src={cover_img} alt="" />
+               <div className='flex justify-between '>
+                    <div className='flex  mt-6 mb-4 '>
+                         <img src={author_img} alt="" />
+                         <div className='ml-6'>
+                              <h3 className='text-base font-bold'>{author}</h3>
+                              <p className='font-semibold'>{posted_date}</p>
+                         </div>
+                    </div>
+                    <div>
+                         <span className='text-sm font-medium'>{reading_time}  min read</span>
+                    </div>
+               </div>
+               <div className='mb-20'>
+                    <h1 className='font-semibold text-4xl mb-2'>{title}</h1>
+                    <p className='font-medium text-sm px-4 '>
+                         {
+                              hashtags.map((hash, inx) => <span key={inx} > <a className='px-1' href="">{hash}</a> </span>)
+                         }
+                    </p>
+               </div>
+          </main>
      );
 };
 Blog.propTypes = {
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,10 @@ import profile from '../../assets/img/profile.png'
 
 const Header = () => {
      return (
-          <div className='flex justify-between p-4 items-center mx-4 border-b-4'>
+          <header className='flex justify-between p-4 items-center mx-4 border-b-4'>
                <h1 className='font-bold text-3xl text-red-400'>Knowledge Cafe</h1>
                <img src={profile} alt="" />
-          </div>
+          </header>
      );
 };
 
